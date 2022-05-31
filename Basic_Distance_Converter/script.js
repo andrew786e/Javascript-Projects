6d9cc830d7ce05dde04bc8a5e8d42921595050b7
andrew786e <74216051+andrew786e@users.noreply.github.com>
2022-05-31 12:30:49 -0600

    slight changes

--- Basic_Distance_Converter/script.js
+++ Basic_Distance_Converter/script.js
@@ -1,23 +1,25 @@
 (function(){
     document.getElementById('convert').addEventListener('submit',function(event){
+
+        "use strict"
         event.preventDefault() ;
 
         // Gets value from the input form 
-        var distance = document.getElementById('distance').value ;
+        const distance = parseFloat(document.getElementById('distance').value) ;
 
-        distance = parseFloat(distance) ;
+
 
         console.log(distance) ;
 
-        var answer = document.getElementById('answer') ;
+        const answer = document.getElementById('answer') ;
 
         // Returns false for a letters and the number zero
         if(!distance){
             answer.innerHTML = "<h2> Please provide a number <\h2>"
         }else{
-            var conversion = distance * 1.609344 ;
+            const conversion = distance * 1.609344 ;
             // var roundedConversion = (Math.round(conversion * 1000))/ 1000;
-            var roundedConversion = conversion.toFixed(3) ;
+            const roundedConversion = conversion.toFixed(3) ;
             answer.innerHTML = `<h2> ${distance} miles converts to kilometers ${roundedConversion} <\h2>`
         }
     });
